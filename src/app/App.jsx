2a1544d9e024529f7859105e896cc2b2cdb2a60e
Alexander <ixav1@icloud.com>
2019-05-14 18:10:55 +0300

    rebuild v0.0.3

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Navigation from './components/navigation/Navigation.jsx';
 import About from './components/about/About.jsx';
 import Contacts from './components/contacts/Contacts.jsx';
-// import Learn from './components/learn/Learn.jsx';
+import Main from './components/main/Main.jsx';
 
 import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './app.css';
@@ -32,7 +32,6 @@ class App extends React.Component {
         <div>
           <Switch>
             <Route path='/' component={MainAppWindow}/>
-            {/* <Redirect exact from='/' to='/monitoring'/> */}
           </Switch>
         </div>
       </Router>
@@ -44,16 +43,15 @@ const MainAppWindow = ({match}) => (
   <div>
     <Navigation url={match.url}/>
     <Switch>
+      <Route path={`${match.url}/`}>
+        {Component(<Main/>)}
+      </Route>
       <Route path={`${match.url}about`}>
         {Component(<About/>)}
       </Route>
       <Route path={`${match.url}contacts`}>
         {Component(<Contacts/>)}
       </Route>
-      {/* <Route path={`${match.url}learn`}>
-        {Component(<Learn/>)}
-        <div>Learn</div>
-      </Route> */}
     </Switch>
   </div>
 );
@@ -61,7 +59,7 @@ const MainAppWindow = ({match}) => (
 // MB - stands for Moonlight Blog
 const Component = (component) => {
   return (
-    <div className='MB-main-container' style={{height: window.innerHeight - 56 + 'px'}}>
+    <div className='mb-main-container'>
       {component}
     </div>
   );
